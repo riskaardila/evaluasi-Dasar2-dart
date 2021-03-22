08bee78a8103bba159f8da8dc47a006075c3c092
riskaardila <riskaardila35@gmail.com>
2021-03-22 16:54:01 +0700

    tambahkan

---
+++ dasar2_dart/bin/dasar2_dart3.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //tunjangan aanak
+  print("Algoritma Menghitung_Tunjangan");
+  int iGajiPokok = int.tryParse(stdin.readLineSync());
+  int iJmlAnak = int.tryParse(stdin.readLineSync());
+  double iTunjangan;
+
+  if (iJmlAnak <= 3 && iJmlAnak > 0) {
+    iTunjangan = iJmlAnak * 0.1 * iGajiPokok;
+  } else if (iJmlAnak > 3) {
+    iTunjangan = 3 * 0.1 * iGajiPokok;
+  } else {
+    iTunjangan = 0;
+  }
+  print("Tunjangan= " + iTunjangan.toString());
+}
